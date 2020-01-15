bf805a1726a7d5577906873da3cab8282d0d6d1e
Cedric <50961672+CCedDev@users.noreply.github.com>
2020-01-15 14:59:17 +0100

    Add user login

--- database.js
+++ database.js
@@ -4,7 +4,8 @@ const mysql = require('mysql');
 var Database_Connection = mysql.createConnection({
     host: config.Database_Host,
     user: config.Database_User,
-    password: config.Database_Password
+    password: config.Database_Password,
+    database: config.Database_Database
     });
 
 Database_Connection.connect(function(err){
@@ -16,13 +17,20 @@ Database_Connection.connect(function(err){
 });
 
 function databaseGetUser(user,password){
-    query = "SELECT ID FROM Users WHERE user = "+ user + " AND password = " +password;
-    Database_Connection.query(query,function(err, result){
-        if(err){
-            throw err;
-        }else{
-            console.log("[Debug]Result " + result);
-        }
+    return new Promise(res => {
+        query = "SELECT ID FROM Users WHERE username = '"+ user + "' AND password = '" +password + "';";
+        console.log(query);
+        Database_Connection.query(query,function(err, result, fields, rows){
+            if(err){
+                throw err;
+            }else{
+                if(result.length === 0){
+                    res(false);
+                }else{
+                    res(true);
+                }
+            }
+        });
     });
 }
 
--- index.js
+++ index.js
@@ -2,14 +2,26 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const config = require('./config.json');
 const Database = require('./database.js')
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-  Database.databaseGetUser("test","test");
+  res.send('Easy Discord Bot\n Version: ' + config.version);
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
+});
+
+app.post('/login', async (req, res) => {
+  console.log('Got body:', req.body.username);
+  let user = await Database.databaseGetUser(req.body.username,req.body.password);
+  console.log(user);
+  if(!user){
+    res.send('No!');
+  }else{
+    res.send('Yes!');
+  }
 });
